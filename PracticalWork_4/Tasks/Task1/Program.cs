572eaf8b0e712921e469a71b2913d1fec5e09bad
Кирилл Талибаев <163425382+Kirif-T@users.noreply.github.com>
2024-10-22 18:14:14 +0400

    Add files via upload

---
+++ PracticalWork_4/Tasks/Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows, columns;
+            Console.Write("Ведите количество строк в матрице: ");
+            rows = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество столбцов в матрице: ");
+            columns = int.Parse(Console.ReadLine());
+
+            // Создание матрицы
+            int[,] matrix = new int[rows, columns];
+
+            // Заполнение матрицы случайными числами
+            Random random = new Random();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = random.Next(100);
+                }
+            }
+
+            // Вывод матрицы на экран
+            Console.WriteLine("Матрица:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{matrix[i,j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            // Сумма элементов матрицы
+            int sum = matrix.Cast<int>().Sum();
+
+            // Вывод суммы на экран
+            Console.WriteLine($"Сумма всех элементов: {sum}");
+        }
+    }
+}
---
+++ PracticalWork_4/Tasks/Task2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows, columns;
+            Console.Write("Введите количество строк в матрице: ");
+            rows = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество столбцов в матрице: ");
+            columns = int.Parse(Console.ReadLine());
+
+            // Создание двух матриц
+            int[,] matrixA = new int[rows, columns];
+            int[,] matrixB = new int[rows, columns];
+            int[,] matrixC = new int[rows, columns];  // для результата
+
+            // Заполнение матриц случайными числами
+            Random random = new Random();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    matrixA[i, j] = random.Next(100);  // первая матрица
+                    matrixB[i, j] = random.Next(100);  // вторая матрица
+                    matrixC[i, j] = matrixA[i, j] + matrixB[i, j];  // сумма
+                }
+            }
+
+            // Вывод первой матрицы на экран
+            Console.WriteLine("\nПервая матрица:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{matrixA[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            // Вывод второй матрицы на экран
+            Console.WriteLine("\nВторая матрица:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{matrixB[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            // Вывод суммы матриц на экран
+            Console.WriteLine("\nСумма матриц:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{matrixC[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            // Сумма всех элементов матрицы C
+            int sumC = matrixC.Cast<int>().Sum();
+            Console.WriteLine($"\nСумма всех элементов матрицы суммы: {sumC}");
+        }
+    }
+}
